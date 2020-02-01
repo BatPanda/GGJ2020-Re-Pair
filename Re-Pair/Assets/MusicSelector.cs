19d20f70a74520cbe79878f7554f827e858345e6
Evey <evey2.watson@live.uwe.ac.uk>
2020-02-01 13:09:29 +0000

    Setup changes

---
+++ Re-Pair/Assets/MusicSelector.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicSelector : MonoBehaviour
+{
+    private bool enabled = false;
+
+    private int controllerNumber;
+    private float inputDelay = 0f;
+
+    public MusicSelection[] musicSelections;
+    private int currentSelection;
+
+    public int numMusicCols;
+    public int numMusicRows;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GetComponentInChildren<Image>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inputDelay <= 0f && enabled)
+        {
+            MoveCursorHorizontal(Input.GetAxis("Horizontal" + controllerNumber));
+            MoveCursorVertical(Input.GetAxis("Vertical" + controllerNumber));
+        }
+        else
+        {
+            inputDelay -= Time.deltaTime;
+        }
+    }
+
+    public void EnableCursor(int controllerIndex)
+    {
+        enabled = true;
+        controllerNumber = controllerIndex;
+        GetComponentInChildren<Image>().enabled = true;
+        currentSelection = 0;
+        transform.position = musicSelections[0].transform.position;
+    }
+
+    void MoveCursorHorizontal(float horizontal)
+    {
+        if (horizontal != 0)
+        {
+            if(horizontal > 0)
+            {
+                if ((currentSelection + 1) % numMusicCols == 0)
+                {
+                    currentSelection -= (numMusicCols-1);
+                }
+                else
+                {
+                    currentSelection++;
+                }
+            }
+            
+            inputDelay = 0.2f;
+        }
+    }
+    void MoveCursorVertical(float vertical)
+    {
+        if (vertical < 0)
+        {
+            if ((currentSelection + numMusicCols) > musicSelections.Length-1)
+            {
+                currentSelection -= numMusicCols;
+            }
+            else
+            {
+                currentSelection += numMusicCols;
+            }
+            Debug.Log(currentSelection);
+        }
+        inputDelay = 0.2f;
+    }
+}
---
+++ Re-Pair/Assets/SetupScreen.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SetupScreen : MonoBehaviour
+{
+    public GameSettings gameSettings;
+
+
+    public DetectController[] detectControllers;
+    public MusicSelector[] musicSelectors;
+
+    public GameObject musicSelectionGroup;
+
+    private List<int> detectedControllers = new List<int>();
+    private int controllersConnected = 0;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (!gameSettings)
+        {
+            gameSettings = FindObjectOfType<GameSettings>();
+        }
+
+        MusicSelection[] musicSelections = musicSelectionGroup.GetComponentsInChildren<MusicSelection>();
+        for(int i = 0; i<musicSelections.Length; i++)
+        {
+            musicSelections[i].musicIndex = i;
+        }
+
+        foreach(MusicSelector musicSelector in musicSelectors)
+        {
+            musicSelector.musicSelections = musicSelections;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 1; i < 7; i++)
+        {
+            if (Input.GetButtonDown("Start" + i) && !detectedControllers.Contains(i))
+            {
+                detectedControllers.Add(i);
+                gameSettings.playerSettings[controllersConnected].connected = true;
+                gameSettings.playerSettings[controllersConnected].playerNum = i;
+                detectControllers[controllersConnected].AddPlayer(i);
+                musicSelectors[controllersConnected].EnableCursor(i);
+                controllersConnected++;
+            }
+        }
+
+        if (detectedControllers.Count > 0)
+        {
+            if (Input.GetButtonDown("Fire" + detectedControllers[0]))
+            {
+                SceneManager.LoadScene(1);
+            }
+        }
+    }
+}
