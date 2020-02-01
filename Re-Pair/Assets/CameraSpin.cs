731a08ec33a54e13abbcce727c61859f62037f92
Evey <evey2.watson@live.uwe.ac.uk>
2020-02-01 18:59:47 +0000

    Music Menu rocks now

---
+++ Re-Pair/Assets/CameraSpin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSpin : MonoBehaviour
+{
+    public float spinSpeed = 0.1f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, spinSpeed);
+
+    }
+}
