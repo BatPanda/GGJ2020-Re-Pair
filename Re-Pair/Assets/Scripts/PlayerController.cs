17343d2d4c8179ff620296a767909db09b24d13e
Aaron <aaronjameslott@gmail.com>
2020-02-01 10:52:48 +0000

    PayerController script started

---
+++ Re-Pair/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float movementSpeed = 4.0f;
+
+    public enum players {player_1, player_2, player_3, player_4};
+
+    public players playerNumber = players.player_1;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        MovementManager(playerNumber);
+    }
+
+    void MovementManager(players playerNo)
+    {
+        string[] splitString = playerNumber.ToString().Split('_');
+
+        float moveHorizontal = Input.GetAxisRaw("Horizontal" + splitString[1].ToString());
+        float moveVertical = Input.GetAxisRaw("Vertical" +  splitString[1].ToString());
+
+        transform.position += new Vector3(movementSpeed * moveHorizontal, movementSpeed * moveVertical, 0) * Time.deltaTime;
+
+    }
+
+    public players getPlayerNumber()
+    {
+        return playerNumber;
+    }
+}
