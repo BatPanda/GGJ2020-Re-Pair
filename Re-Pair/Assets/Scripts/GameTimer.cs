5957c725895add751cce6ea326a81e14586c3dbd
Evey <evey2.watson@live.uwe.ac.uk>
2020-02-02 12:41:17 +0000

    Toggle Pause

--- Re-Pair/Assets/Scripts/GameTimer.cs
+++ Re-Pair/Assets/Scripts/GameTimer.cs
@@ -10,6 +10,8 @@ public class GameTimer : MonoBehaviour
     public float endGameTimer = 10f;
     public float panicTime = 30f;
 
+    bool paused = false;
+
     int panicState = 0;
 
     public Text gameTimer;
@@ -25,7 +27,7 @@ public class GameTimer : MonoBehaviour
             gamePlaying = false;
             StartCoroutine(EndGame());
         }
-        else
+        else if (!paused)
         {
             timeElapsed += Time.deltaTime;
             if (gameTimeSeconds - timeElapsed < panicTime && panicState == 0)
@@ -72,5 +74,10 @@ public class GameTimer : MonoBehaviour
 
         SceneManager.LoadScene(0);
         yield break;
+    }
+
+    public void TogglePause()
+    {
+        paused = !paused;
     }
 }
